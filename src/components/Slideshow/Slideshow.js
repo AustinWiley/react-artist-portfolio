9138aaa42cee5278423a7f1144b92908a80d8d68
Austin WIley <wileybeer@gmail.com>
2020-01-19 19:50:13 -0700

    js added to slideshow

--- src/components/Slideshow/Slideshow.js
+++ src/components/Slideshow/Slideshow.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import Slide from "../Slide/Slide"
 
 class Slideshow extends Component {
   constructor() {
@@ -6,17 +7,60 @@ class Slideshow extends Component {
     this.state = {
       pigeons: ["Austin", "Sarah", "Emmry", "Haven"]
     }
+
+    var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
+
   }
   render() {
     return (
-        <React.Fragment>
-        <p>This is the slide show</p>
-        <ul>
-          {this.state.pigeons.map(userName => {
-            return <li>{userName}</li>
-          })}
-        </ul>
-        </React.Fragment>
+      <>
+<div className="slideshow-container">
+
+  
+  {this.state.pigeons.map(userName => {
+            return <Slide caption={userName} />
+  })}
+
+  
+  <a className="prev" onclick={this.plusSlides(-1)}>&#10094;</a>
+  <a class="next" onclick={this.plusSlides(1)}>&#10095;</a>
+</div>
+<br />
+
+
+<div style="text-align:center">
+  <span className="dot" onclick="currentSlide(1)"></span>
+  <span classNamer="dot" onclick="currentSlide(2)"></span>
+  <span className="dot" onclick="currentSlide(3)"></span>
+</div>
+</>
     )
   }
 }
